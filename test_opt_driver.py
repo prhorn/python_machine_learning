696b1b92b5b1b483668c2cf300400dd5e97a2f60
Paul Horn <prhorn@airbears2-10-142-63-18.AirBears2.1918.Berkeley.EDU>
2015-10-01 14:03:44 -0700

    l_bfgs seems to work

--- test_opt_driver.py
+++ test_opt_driver.py
@@ -2,13 +2,15 @@
 
 from quadratic_test import quadratic_test
 from steepest_descent import steepest_descent
+from l_bfgs import l_bfgs
 
 max_iter = 4000
-problem_size = 2
+problem_size = 10
 
 problem = quadratic_test(problem_size)
 problem.obtain_guess()
-optimizer = steepest_descent(problem)
+#optimizer = steepest_descent(problem)
+optimizer = l_bfgs(problem)
 
 converged = False
 cur_iter = 0
@@ -17,9 +19,9 @@ for i in range(max_iter):
    converged = optimizer.next_step() 
    if (converged):
       break
-   print "  "+str(cur_iter)+"  "+str(problem.value())+"  "+str(optimizer.error)+"  "+optimizer.comment
+   print "  "+str(cur_iter)+"  "+str(optimizer.value)+"  "+str(optimizer.error)+"  "+optimizer.comment
 
 if (converged):
-   print "  "+str(cur_iter)+"  "+str(problem.value())+"  "+str(optimizer.error)+"  Optimization Converged"
+   print "  "+str(cur_iter)+"  "+str(optimizer.value)+"  "+str(optimizer.error)+"  Optimization Converged"
 else:
    print 'optimization failed to converge'
